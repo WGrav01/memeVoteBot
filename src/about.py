843ad6dca6c25d46215936f667d5103683128539
WGrav01 <63617338+WGrav01@users.noreply.github.com>
2024-12-14 13:27:40 -0500

    Formatted about.py

--- src/about.py
+++ src/about.py
@@ -4,6 +4,7 @@ from src import Bot
 
 bot = Bot()
 
+
 @bot.group()
 async def about():
     pass
@@ -25,10 +26,14 @@ async def ping(ctx):
     pong = discord.Embed(title="Pong!", description=f"{ping}ms", color=color)
     await ctx.respond(embed=pong, ephemeral=False)
 
+
 @about.slash_command(name="github", description="get the link to the bot's github")
 async def github(ctx):
     await ctx.defer(ephemeral=False)
     github = "https://github.com/WGrav01/memeVoteBot"
     response = discord.Embed(title="memeVoteBot's Github", description=f"{github}")
-    about = response.add_field(name="About", value="Made with love by wgrav, open source under the AGPLv3 license.")
+    about = response.add_field(
+        name="About",
+        value="Made with love by wgrav, open source under the AGPLv3 license.",
+    )
     await ctx.respond(embed=response, ephemeral=False)
